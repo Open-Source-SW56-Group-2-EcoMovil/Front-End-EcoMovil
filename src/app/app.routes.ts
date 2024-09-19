b6f895bf0f397719d43d0dc890e317ddd0003785
Beth Oneglio <106390542+bethnlg@users.noreply.github.com>
2024-09-19 02:06:13 -0500

    feat: add register university page component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {VehiclesComponent} from "./public/pages/vehicles/vehicles.component";
 import {VehiclePostComponent} from "./public/pages/vehicle-post/vehicle-post.component";
+import {RegisterUniversityStudentComponent} from "./public/pages/register-university-student/register-university-student.component";
 
 export const routes: Routes = [
   {path:'myVehicles', component: VehiclesComponent},
-  {path: 'postVehicle', component: VehiclePostComponent}
+  {path: 'postVehicle', component: VehiclePostComponent},
+  {path: '', component: RegisterUniversityStudentComponent}
 ];
