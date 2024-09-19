e35baedda6eb57b009389a20246b540f7d1a7360
Beth Oneglio <106390542+bethnlg@users.noreply.github.com>
2024-09-19 02:48:17 -0500

    feat: add register acquirer.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {VehiclesComponent} from "./public/pages/vehicles/vehicles.component";
 import {VehiclePostComponent} from "./public/pages/vehicle-post/vehicle-post.component";
+import {RegisterAcquirerComponent} from "./public/pages/register-acquirer/register-acquirer.component";
 
 export const routes: Routes = [
   {path:'myVehicles', component: VehiclesComponent},
-  {path: 'postVehicle', component: VehiclePostComponent}
+  {path: 'postVehicle', component: VehiclePostComponent},
+  {path: 'registerAcquirer', component: RegisterAcquirerComponent},
 ];
