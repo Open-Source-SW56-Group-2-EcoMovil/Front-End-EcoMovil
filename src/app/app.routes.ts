43fee818653e659356225a249ebb3ac05b3ac562
Beth Oneglio <106390542+bethnlg@users.noreply.github.com>
2024-09-19 02:32:42 -0500

    feat: add log in.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {VehiclesComponent} from "./public/pages/vehicles/vehicles.component";
 import {VehiclePostComponent} from "./public/pages/vehicle-post/vehicle-post.component";
+import {LogInComponent} from "./public/pages/log-in/log-in.component";
 
 export const routes: Routes = [
   {path:'myVehicles', component: VehiclesComponent},
-  {path: 'postVehicle', component: VehiclePostComponent}
+  {path: 'postVehicle', component: VehiclePostComponent},
+  {path: 'logIn', component: LogInComponent},
 ];
