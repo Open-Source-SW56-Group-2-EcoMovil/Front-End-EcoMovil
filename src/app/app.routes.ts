bc4840352d6e2dbf7a9e376cb984aa3dc54d65ef
Juan Calisaya <u202121935@upc.edu.pe>
2024-09-24 18:29:22 -0500

    feat(server): update db.json for filter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import {VehiclesComponent} from "./public/pages/vehicles/vehicles.component";
 import {VehiclePostComponent} from "./public/pages/vehicle-post/vehicle-post.component";
-
+import {FilterAcquirerComponent} from "./public/pages/filter-acquirer/filter-acquirer.component";
 export const routes: Routes = [
   {path:'myVehicles', component: VehiclesComponent},
-  {path: 'postVehicle', component: VehiclePostComponent}
+  {path: 'postVehicle', component: VehiclePostComponent},
+  {path: 'filter', component: FilterAcquirerComponent}
 ];
